fbb94db8be21b9f94bff5c2baddb03dfebd50888
Adam Njegovanovic <adamnj@kth.se>
2023-04-16 17:13:56 +0200

    script

--- gameScript.js
+++ gameScript.js
@@ -1,23 +1,27 @@
 const GAME_BOARD_WIDTH = 10;
 const GAME_BOARD_HEIGHT = 20;
 let gameBoard = [];
-for (let y = 0; y < GAME_BOARD_HEIGHT; y++) {
-    let row = [];
-    for (let x = 0; x < GAME_BOARD_WIDTH; x++) {
-        row.push(0);
-    }
-    gameBoard.push(row);
-}
 
+function createGameBoard() {
+    for (let y = 0; y < GAME_BOARD_HEIGHT; y++) {
+        let row = [];
+        for (let x = 0; x < GAME_BOARD_WIDTH; x++) {
+            row.push(0);
+        }
+        gameBoard.push(row);
+    }
+}
 function drawGameBoard() {
     const gameBoardElement = document.getElementById("gameBoard");
     gameBoardElement.innerHTML = "";
     for (let y = 0; y < GAME_BOARD_HEIGHT; y++) {
         for (let x = 0; x < GAME_BOARD_WIDTH; x++) {
             const square = document.createElement("div");
-            square.className = gameBoard[y][x] ? "square filled" : "square";
+            square.className = gameBoard[y][x] ? "squareFilled" : "square";
             gameBoardElement.appendChild(square);
+
         }
     }
 }
+createGameBoard();
 drawGameBoard();
